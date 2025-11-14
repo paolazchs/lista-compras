881f95fb8809186f5d05d17923a0ec42905b6ea0
paolazchs <paola9liveira06@gmail.com>
2025-11-14 19:57:26 -0300

    Projeto finalizado

--- src/app/lista/lista.ts
+++ src/app/lista/lista.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { ItemLista } from './itemlista';
 import { FormsModule } from '@angular/forms'
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-lista',
-  imports: [ FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './lista.html',
 })
 
@@ -18,14 +18,21 @@ export class Lista {
     let itemLista = new ItemLista();
     itemLista.nome = this.item;
     itemLista.id =  this.lista.length + 1;
-    itemLista.comprado;
 
     this.lista.push(itemLista);
 
     this.item = '';
 
     console.table(this.lista);
+  }
 
+  riscarItem( itemLista: ItemLista){
+    itemLista.comprado = !itemLista.comprado;
+  }
+
+  limparLista( ){
+    this.lista = [];
   }
 
 }
+
