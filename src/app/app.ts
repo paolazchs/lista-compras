0ae8cd199ce875095cc626f444a31395f05e15aa
paolazchs <paola9liveira06@gmail.com>
2025-11-13 22:14:09 -0300

    primeiro commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Lista } from './lista/lista';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Lista],
   templateUrl: './app.html',
 })
 export class App {
-  
+
 }
--- src/app/lista/lista.ts
+++ src/app/lista/lista.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { ItemLista } from './itemlista';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-lista',
-  imports: [],
+  imports: [ FormsModule],
   templateUrl: './lista.html',
 })
+
+
 export class Lista {
+
+  item: string = '';
+  lista: ItemLista[] = [];
+
+  adicionarItem() {
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id =  this.lista.length + 1;
+    itemLista.comprado;
+
+    this.lista.push(itemLista);
+
+    this.item = '';
+
+    console.table(this.lista);
+
+  }
 
 }
